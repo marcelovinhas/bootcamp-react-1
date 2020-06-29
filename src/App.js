7e76cd3b797e6db2261c81ce1a3cb638d7806bb7
marcelovinhas <marcelovinhascosta@hotmail.com>
2020-06-29 12:25:05 -0300

    6,7 - Componentes e estado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 
-import profile from './assets/Foto.jpg';
+import Techlist from './components/TechList';
+
+/*
+há várias formas de escrever um component em React, no formato de função como está aqui, ou com class em TechList.js
+no reactJS em vez usar const variável = []; usa estados, quando uma variável for manipulada é chamada de estado
+há pouco tempo atrás não podia ter estado dentro de função, por isso usava class, inicialmente vamos usar class
+e depois passar para função
+*/
 
 function App() {
-  return <img width="200" src={profile} />
+  return <Techlist />
 }
 
 export default App;
